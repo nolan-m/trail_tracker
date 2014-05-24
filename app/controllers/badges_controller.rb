9a93aca24c292b53d0a7f3e77234c7abb52ce32d
Nolan Miller <namiller2@gmail.com>
2014-05-24 15:35:27 -0700

    add badges, trails, users controller

---
+++ app/controllers/badges_controller.rb
@@ -0,0 +1,9 @@
+class BadgesController < ApplicationController
+	def index
+		@badges = Badge.all
+	end
+
+  def show
+    @badge = Badge.find(params[:id])
+  end
+end
---
+++ app/controllers/trails_controller.rb
@@ -0,0 +1,11 @@
+class TrailsController < ApplicationController
+	def index
+		@trails = Trail.all
+	end
+
+  def show
+    @trail = Trail.find(params[:id])
+  end
+
+
+end
