dda7d728c10c10e55f5b47471f684f556b810f53
Nolan Miller <namiller2@gmail.com>
2014-05-25 11:29:46 -0700

    ability to create new trails

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -7,5 +7,23 @@ class TrailsController < ApplicationController
     @trail = Trail.find(params[:id])
   end
 
+  def new
+  	@trail = Trail.new
+  end
+
+  def create
+  	@trail = Trail.new(trail_params)
+  	if @trail.save
+  		flash[:notice] = "Trail created."
+      redirect_to trails_path
+    else
+      render 'new'
+    end
+  end
+
+private
+  def trail_params
+    params.require(:trail).permit(:name, :length, :difficulty, :location, :loop_id, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Trails::Application.routes.draw do
   devise_for :users
   root to: "welcome#index"
   resources :users, only: [:show]
-  resources :trails, only: [:show, :index]
+  resources :trails, only: [:new, :create, :show, :index]
   resources :badges, only: [:show, :index]
   resources :loops, only: [:show, :index]
 end
