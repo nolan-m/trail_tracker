ea8ec0a4adc8a707ea6cd3511772a3aae30aaacc
Nolan Miller <namiller2@gmail.com>
2014-05-25 11:51:24 -0700

    ability to add badges"

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -6,4 +6,24 @@ class BadgesController < ApplicationController
   def show
     @badge = Badge.find(params[:id])
   end
+
+  def new
+  	@badge = Badge.new
+  end
+
+  def create
+  	@badge = Badge.new(badge_params)
+  	if @badge.save
+  		flash[:notice] = "Badge created."
+      redirect_to badges_path
+    else
+      render 'new'
+    end
+  end
+
+private
+  def badge_params
+    params.require(:badge).permit(:name, :requirements)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Trails::Application.routes.draw do
   root to: "welcome#index"
   resources :users, only: [:show]
   resources :trails, only: [:new, :create, :show, :index]
-  resources :badges, only: [:show, :index]
+  resources :badges, only: [:new, :create, :show, :index]
   resources :loops, only: [:new, :create, :show, :index]
 end
