9af6b42b727fa8de0c58d7b29432eb6bcefb082f
Nolan Miller <namiller2@gmail.com>
2014-05-25 15:49:26 -0700

    validate image presence for badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -5,6 +5,7 @@ class Badge < ActiveRecord::Base
 	validates_presence_of :name
 	validates_uniqueness_of :name
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :image
 end
--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -5,4 +5,6 @@ describe Badge do
   it { should have_and_belong_to_many :users }
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
+  it { should validate_attachment_presence :image }
+  it { should have_attached_file :image }
 end
